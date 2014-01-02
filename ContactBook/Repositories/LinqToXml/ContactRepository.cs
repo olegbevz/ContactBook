055f0fc542f1591f3e6679f50bb684b4d4761fc4
olegbevz <olegdb@inbox.ru>
2014-01-02 21:12:47 +0700

    add contactrepository

---
+++ ContactBook/Repositories/LinqToXml/ContactRepository.cs
@@ -0,0 +1,41 @@
+﻿using ContactBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactBook.Repositories.LinqToXml
+{
+    public class ContactRepository : IContactRepository
+    {
+        public Contact Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Contact> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
