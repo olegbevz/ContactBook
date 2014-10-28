6e6b5ca995d92e3c697b3797f4c5e7cc33e2bd22
olegbevz <olegdb@inbox.ru>
2014-10-28 21:08:08 +0700

    delete Web API nuget paket

--- ContactBook/Global.asax.cs
+++ ContactBook/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ContactBook
@@ -16,7 +11,6 @@ namespace ContactBook
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
